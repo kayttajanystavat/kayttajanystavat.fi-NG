9744edc1159e717531519630312f25be28a814fe
Emil Virkki <emil.virkki@gmail.com>
2014-06-11 17:40:05 +0300

    Changed drushrc.php to download modules to sites/all instead of sites/default

--- sites/default/drushrc.php
+++ sites/default/drushrc.php
@@ -2,4 +2,4 @@
 // Drush configuration file, for examples see http://drush.ws/examples/example.drushrc.php
 
 //Download modules to contrib subdirectory for separation
-$command_specific['dl'] = array('destination' => 'sites/default/modules/contrib');
+$command_specific['dl'] = array('destination' => 'sites/all/modules/contrib');
